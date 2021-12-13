e2d7f26202a9eaf66836d0902c0ae99be1c1fb8f
ZzAZz4 <esteban.villacorta@utec.edu.pe>
2021-12-13 02:36:26 -0500

    a

--- main.cpp
+++ main.cpp
@@ -1,13 +1,11 @@
 #include <iostream>
 #include <vector>
-#include <memory>
 #include <cmath>
 #include <cassert>
 #include <bitset>
 #include "Timer.hpp"
 
-#define NUM_THREADS 8
-#define SCHEDULE_MAX_DEPTH 10
+#define NUM_THREADS 4
 #define USE_CRITICAL
 //#define USE_REDUCE
 
@@ -17,7 +15,7 @@ namespace BBSeq {
 
     void
     summon_solve (
-        const Matrix& dist, uint8_t pos, uint32_t set, float path_length, /*in*/
+        const Matrix& dist, uint8_t pos, uint16_t set, float path_length, /*in*/
         float& best /*out*/
     ) {
         if (set == 0) { /* nowhere else to go: return to 0 and call it a day */
@@ -51,39 +49,10 @@ namespace BBPar {
     using Matrix = std::vector<std::vector<float>>;
 
     void
-    summon_solve (
-        const Matrix& dist, uint8_t pos, uint32_t set, float path_length, /*in*/
-        float& best /*out*/
-        ) {
-        if (set == 0) { /* nowhere else to go: return to 0 and call it a day */
-            auto length = path_length + dist[pos][0];
-#ifdef USE_CRITICAL
-#pragma omp critical
-#endif
-            if (length < best) {
-                best = length;
-            }
-        }
-        for (std::size_t next = 0; next < dist.size(); ++next) {
-            if (set & (1u << next)) { /* if next position is usable */
-                const auto extended_len = path_length + dist[pos][next];
-                if (extended_len < best) { /* extend the path */
-                    summon_solve(dist, next, set & ~(1u << next), extended_len, best);
-                }
-            }
-        }
-    }
-
-    void
     summon_solve_par (
-        const Matrix& dist, uint8_t pos, uint32_t set, float path_length, /*in*/
-        float& best /*out*/,
-        int depth = 0
+        const Matrix& dist, uint8_t pos, uint16_t set, float path_length, /*in*/
+        float& best /*out*/
     ) {
-        if (depth >= SCHEDULE_MAX_DEPTH) {
-            summon_solve(dist, pos, set, path_length, best);
-        }
-
         if (set == 0) { /* nowhere else to go: return to 0 and call it a day */
             auto length = path_length + dist[pos][0];
 #ifdef USE_CRITICAL
@@ -95,9 +64,9 @@ namespace BBPar {
         }
 
 #if defined (USE_CRITICAL)
-#pragma omp parallel for default(none) shared(dist, pos, path_length, set, depth) shared(best)  num_threads(NUM_THREADS) schedule(dynamic)
+#pragma omp parallel for default(none) shared(dist, pos, path_length, set) shared(best)  num_threads(NUM_THREADS) schedule(dynamic)
 #elif defined (USE_REDUCE)
-#pragma omp parallel for default(none) shared(dist, pos, path_length, set, depth) num_threads(NUM_THREADS) reduction(min: best) schedule(dynamic)
+#pragma omp parallel for default(none) shared(dist, pos, path_length, set) num_threads(NUM_THREADS) reduction(min: best) schedule(dynamic)
 #else
 #error No strategy used
 #endif
@@ -105,13 +74,13 @@ namespace BBPar {
             if (set & (1u << next)) { /* if next position is usable */
                 const auto extended_len = path_length + dist[pos][next];
                 if (extended_len < best) { /* extend the path */
-                    summon_solve_par(dist, next, set & ~(1u << next), extended_len, best, depth + 1);
+                    summon_solve_par(dist, next, set & ~(1u << next), extended_len, best);
                 }
             }
         }
     }
 
-    float solve (const Matrix& distances) {
+    float solve (Matrix& distances) {
         assert(!distances.empty());
         assert(distances.size() == distances[0].size());
 
